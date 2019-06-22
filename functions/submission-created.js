6c4d05f0248e31c697cfa8c960d323ed6162cd7d
Matthew Ström <matthew.h.strom@gmail.com>
2019-06-21 22:21:40 -0400

    fetch via require

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,13 +1,13 @@
-import fetch from 'node-fetch'
+const fetch = require('node-fetch')
 const { EMAIL_TOKEN } = process.env
 const API_ENDPOINT = 'https://api.buttondown.email/v1/subscribers'
 
 exports.handler = async (event, context, callback) => {
-    const email = JSON.parse(event.body).email;
+    const email = JSON.parse(event.body).email
     return fetch(API_ENDPOINT, {
         method: 'POST',
         headers: { Authorization: `Token ${EMAIL_TOKEN}` },
-        body: JSON.stringify({email})
+        body: JSON.stringify({ email }),
     })
         .then(response => response.json())
         .catch(error => ({ statusCode: 422, body: String(error) }))
