34524982717e0a8290c01984476e9980dbd64538
Matthew Ström <matthew.h.strom@gmail.com>
2019-06-21 22:39:22 -0400

    get email right, set content type

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,16 +3,19 @@ const { EMAIL_TOKEN } = process.env
 const API_ENDPOINT = 'https://api.buttondown.email/v1/subscribers'
 
 exports.handler = async (event, context, callback) => {
-    const email = JSON.parse(event.body).payload
-    console.log(email);
+    const email = JSON.parse(event.body).payload.email
+    console.log(email)
     return fetch(API_ENDPOINT, {
         method: 'POST',
-        headers: { Authorization: `Token ${EMAIL_TOKEN}` },
+        headers: {
+            Authorization: `Token ${EMAIL_TOKEN}`,
+            'Content-Type': 'application/json',
+        },
         body: JSON.stringify({ email }),
     })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            console.log(data)
         })
         .catch(error => ({ statusCode: 422, body: String(error) }))
 }
