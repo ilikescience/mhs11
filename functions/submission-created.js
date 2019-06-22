d215e9d2ef8cf19e778d3cc350b9683d4fe7d6cd
Matthew Ström <matthew.h.strom@gmail.com>
2019-06-21 22:30:17 -0400

    try logging everything

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,15 +3,16 @@ const { EMAIL_TOKEN } = process.env
 const API_ENDPOINT = 'https://api.buttondown.email/v1/subscribers'
 
 exports.handler = async (event, context, callback) => {
-    const email = JSON.parse(event.body).email
+    const email = JSON.parse(event.body)
     console.log(email);
     return fetch(API_ENDPOINT, {
         method: 'POST',
         headers: { Authorization: `Token ${EMAIL_TOKEN}` },
         body: JSON.stringify({ email }),
     })
-        .then(response => {
-            console.log(response.json())
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
         })
         .catch(error => ({ statusCode: 422, body: String(error) }))
 }
