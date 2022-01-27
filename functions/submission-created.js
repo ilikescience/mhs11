875c66fb0eabc925417f0392f177e27ab2bd0598
Matthew Ström <matthew.h.strom@gmail.com>
2022-01-27 16:25:44 -0500

    temporarily fix email submissions I think

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,19 +1,23 @@
-const fetch = require('node-fetch')
-const { EMAIL_TOKEN } = process.env
-exports.handler = async event => {
-    const email = JSON.parse(event.body).payload.email
-    console.log(`Recieved a submission: ${email}`)
-    return fetch('https://api.buttondown.email/v1/subscribers', {
-        method: 'POST',
-        headers: {
-            Authorization: `Token ${EMAIL_TOKEN}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(`Submitted to Buttondown:\n ${data}`)
-        })
-        .catch(error => ({ statusCode: 422, body: String(error) }))
-}
+import fetch from 'node-fetch';
+const { EMAIL_TOKEN } = process.env;
+exports.handler = async (event) => {
+    const email = JSON.parse(event.body).payload.email;
+    console.log(`Recieved a submission: ${email}`);
+    const response = await fetch(
+        'https://api.buttondown.email/v1/subscribers',
+        {
+            method: 'POST',
+            headers: {
+                Authorization: `Token ${EMAIL_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email }),
+        }
+    );
+    let responseText = await response.text();
+    console.log('response:', responseText);
+    return {
+        statusCode: response.status,
+        body: responseText,
+    };
+};
